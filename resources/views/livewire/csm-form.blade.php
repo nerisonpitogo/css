8fe795fd7ab024f3d83fd8125b427d095fea1739
nerisonpitogo <son2xp2go04@gmail.com>
2025-06-24 10:18:42 +0800

    a

--- resources/views/livewire/csm-form.blade.php
+++ resources/views/livewire/csm-form.blade.php
@@ -2431,7 +2431,7 @@ new #[Layout('components.layouts.form')] #[Title('CSM')] class extends Component
             </x-mary-button>
         </div>
         <div class="flex mt-5">
-            <x-mary-theme-toggle darkTheme="{{ env('DARK_THEME', 'forest') }}"
+            <x-mary-theme-toggle darkTheme="{{ env('DARK_THEME', 'dark') }}"
                 lightTheme="{{ env('LIGHT_THEME', 'cupcake') }}" class="btn btn-circle btn-ghost" />
         </div>
     </div>
